68096d86cd8ede4bf0eee8ae68753b73f48714e9
rgreiner1 <remygreiner1@gmail.com>
2023-10-27 20:30:47 +0200

    norme+fix builtin_redi

--- utils/builtins_utils.c
+++ utils/builtins_utils.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   builtins_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rgreiner <rgreiner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:25:53 by rgreiner          #+#    #+#             */
-/*   Updated: 2023/10/27 00:57:28 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/27 20:28:16 by rgreiner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	close_redi(int out, int file)
+void close_redi(int out, int file)
 {
 	if (file != 1)
 	{
@@ -22,9 +22,9 @@ void	close_redi(int out, int file)
 	}
 }
 
-int	ft_search_token(t_lex *lex)
+int ft_search_token(t_lex *lex)
 {
-	t_lex	*tmp;
+	t_lex *tmp;
 
 	tmp = lex;
 	while (tmp)
@@ -36,9 +36,9 @@ int	ft_search_token(t_lex *lex)
 	return (0);
 }
 
-int	ft_multi_redi(t_lex *tmp)
+int ft_multi_redi(t_lex *tmp)
 {
-	t_lex	*tmp2;
+	t_lex *tmp2;
 
 	tmp2 = tmp;
 	tmp2 = tmp2->next;
@@ -53,29 +53,13 @@ int	ft_multi_redi(t_lex *tmp)
 	return (1);
 }
 
-int	ft_builtin_redi2(int file, int child, t_lex *tmp)
+int	ft_builtin_redi2(t_lex *tmp, int file, int mod)
 {
-	if (tmp->next && tmp->type == 3)
-	{
-		file = openfile(tmp->next->content, 1);
-		if (ft_multi_redi(tmp) == 1)
-			return (file);
-		close (file);
-	}
-	if (tmp->next && tmp->type == 5)
-	{
-		file = openfile(tmp->next->content, 0);
-		if (ft_multi_redi(tmp) == 1)
-			return (file);
-		close(file);
-	}
-	if (tmp->next && tmp->type == 2)
-	{
-		if (access(tmp->next->content, F_OK | R_OK) != 0)
-			ft_error(tmp->next->content, ": No such file or directory" \
-				, NULL, child);
-	}
-	return (0);
+	file = openfile(tmp->next->content, mod);
+	if (ft_multi_redi(tmp) == 1)
+		return (file);
+	close(file);
+	return(1);
 }
 
 int	ft_builtin_redi(t_lex *lex, int file, int child)
@@ -85,11 +69,21 @@ int	ft_builtin_redi(t_lex *lex, int file, int child)
 	tmp = lex;
 	while (tmp)
 	{
-		if (ft_builtin_redi2(file, child, tmp) == file)
-			return (file);
+		if (tmp->next && tmp->type == 3)
+			file = ft_builtin_redi2(tmp, file, 1);
+		if (tmp->next && tmp->type == 5)
+			file = ft_builtin_redi2(tmp, file, 0);
+		if (tmp->next && tmp->type == 2)
+		{
+			if (access(tmp->next->content, F_OK | R_OK) != 0)
+				ft_error(tmp->next->content, ": No such file or directory" \
+				, NULL, child);
+		}
 		if (tmp->type == 1)
-			return (file);
+			exit(1);
 		tmp = tmp->next;
+		if(file != 1)
+			return(file);
 	}
 	return (1);
 }
